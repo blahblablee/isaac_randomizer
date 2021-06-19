7f872219e27ec8b82e985e16a0a62c9a5be53636
blahblablee <blahblablee@gmail.com>
2021-06-18 20:06:43 -0400

    Initial functional commit

---
+++ js/generator.js
@@ -0,0 +1,29 @@
+var selectedCharacters = new Array;
+var selectedEndings = new Array;
+
+function generate() {
+    createSelectionSets();
+    var character = selectedCharacters[Math.floor(Math.random()*selectedCharacters.length)];
+    var characterImage = "imgs/character_heads/" + character.toLowerCase().trim() + ".png";
+    var ending = selectedEndings[Math.floor(Math.random()*selectedEndings.length)];
+    var endingImage = "imgs/ending_notes/" + ending.toLowerCase().trim() + ".webp";
+    $("#output").html( "<img id='selectedCharacter'>" + character + " to " + ending + "<img height='25' id='selectedEnding'>");
+    $("#output img").attr("src", characterImage);
+    $("#output #selectedEnding").attr("src", endingImage)
+}
+
+function createSelectionSets() {
+    selectedCharacters = [];
+    $("#characters input").each(function(index) {
+        if ($(this).is(':checked')) {
+            selectedCharacters.push($(this).next().html());
+        }
+    });
+
+    selectedEndings = [];
+    $("#endings input").each(function(index) {
+        if ($(this).is(':checked')) {
+            selectedEndings.push($(this).next().html());
+        }
+    });
+}
